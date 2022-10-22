abe5659e143369f7347d439c9f4cdd024ed083d3
Ekaterina Andreeva <andreevaekatt@gmail.com>
2022-10-22 15:28:56 +0300

    added answer fields

--- src/components/QuestionBox/QuestionBox.jsx
+++ src/components/QuestionBox/QuestionBox.jsx
@@ -4,11 +4,14 @@ import "./QuestionBox.scss";
 const QuestionBox = ({ userInfo }) => {
     return (
       <div className="question_box">
-        <Avatar />
-        <div className="question_box__text">
-          <h3 className="question_box__name">{userInfo.name}</h3>
-          <p className="question_box__question">{userInfo.question}</p>
+        <div className="question_box__user_info">
+          <Avatar />
+          <div className="question_box__text">
+            <h3 className="question_box__name">{userInfo.name}</h3>
+            <p className="question_box__question">{userInfo.question}</p>
+          </div>
         </div>
+        <div className="question_box__answer"></div>
       </div>
     );
 }
