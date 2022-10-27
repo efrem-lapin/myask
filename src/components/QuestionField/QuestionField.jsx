4f855c9c0f4eeee22394b03b69b3826f934c66c2
Ekaterina Andreeva <andreevaekatt@gmail.com>
2022-10-27 21:55:30 +0300

    Added logic for displaying the QuestionField component

---
+++ src/components/QuestionField/QuestionField.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const QuestionField = () => {
+  
+  const [question, setQuestion] = React.useState("")
+
+  return (
+    <div className="question_field">
+      <input 
+        type="textarea" 
+        className="question_field__input" 
+        onChange={(e) => setQuestion(e.target.value)} 
+        value={question}
+      />
+      <button className="question_field__button">Сбросить</button>
+    </div>
+  );
+}
+
+
+
+export default QuestionField
--- src/components/UserBtnPanel/UserBtnPanel.jsx
+++ src/components/UserBtnPanel/UserBtnPanel.jsx
@@ -1,14 +1,23 @@
 import "./UserBtnPanel.scss";
+import QuestionField from "../QuestionField/QuestionField";
+import { useState } from "react";
 
 const UserBtnPanel = () => {
+
+  const [isShowQF, setIsShowQF] = useState(false)
+
   return (
     <div className="container">
       <div className="user_btn_panel">
-        <button className="user_btn_panel__btn user_btn_panel__btn_long">
-          Задать вопрос
+        <button 
+          className="user_btn_panel__btn user_btn_panel__btn_long"
+          onClick={() => setIsShowQF(prev => !prev)}
+          >
+          {isShowQF ? "Скрыть" : "Задать вопрос"}
         </button>
         <button className="user_btn_panel__btn">Подписаться</button>
       </div>
+      {isShowQF && <QuestionField />}
     </div>
   );
 };
